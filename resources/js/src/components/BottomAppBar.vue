503cbe5fb379a2c03c1044bddee85bf68c4bd70a
Amit Patel <95671727+amitpateloffical@users.noreply.github.com>
2025-08-30 01:49:16 +0530

    Fix duplicate icons in app bar - Change watchlist to eye icon and portfolio to document icon

--- resources/js/src/components/BottomAppBar.vue
+++ resources/js/src/components/BottomAppBar.vue
@@ -13,7 +13,7 @@
       <div class="app-bar-item" @click="handleWatchlist()" :class="{ active: activeSection === 'watchlist' }">
         <div class="app-bar-icon">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
+            <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
           </svg>
         </div>
         <span class="app-bar-label">Watchlist</span>
@@ -22,7 +22,7 @@
       <div class="app-bar-item" @click="navigateToPage('/user/portfolio')" :class="{ active: isActive('/user/portfolio') }">
         <div class="app-bar-icon">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
+            <path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z"/>
           </svg>
         </div>
         <span class="app-bar-label">Portfolio</span>
